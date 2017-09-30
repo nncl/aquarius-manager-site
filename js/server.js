fff7da5a0d8cd550a7777c521143f51d92aa88d3
= <nncl@live.com>
2017-09-30 16:56:33 -0300

    Add server

---
+++ js/server.js
@@ -0,0 +1,17 @@
+/**
+ * @description
+ * Server
+ */
+
+const http = require('http')
+    , fs = require('fs');
+
+http.createServer(function (req, res) {
+
+    fs.readFile('index.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+    });
+
+}).listen(process.env.PORT || 8000);
