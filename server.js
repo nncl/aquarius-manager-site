0d97f9d8668a1a1da738a2e688d7081616b6aebd
= <nncl@live.com>
2017-09-30 17:06:26 -0300

    Update server static files

---
+++ server.js
@@ -0,0 +1,18 @@
+/**
+ * @description
+ * Server
+ */
+
+const express = require('express')
+    , app = express()
+    , path = require("path");
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(3000);
+
+console.log("Running at Port 3000");
